4b74cf5daac03ee1b99b5b77fe6523950fb95d5a
Karime Cedano <karimecedano02@gmail.com>
2026-07-26 18:52:41 -0400

    feat: add student list (read)

---
+++ Models/read-students.php
@@ -0,0 +1,40 @@
+﻿<?php
+
+$students = [
+    [
+        "id" => 1,
+        "name" => "Juan Perez",
+        "email" => "juan@email.com"
+    ],
+    [
+        "id" => 2,
+        "name" => "Maria Lopez",
+        "email" => "maria@email.com"
+    ],
+    [
+        "id" => 3,
+        "name" => "Carlos Ruiz",
+        "email" => "carlos@email.com"
+    ]
+];
+
+?>
+
+<h2>Lista de Estudiantes</h2>
+
+<table border="1">
+    <tr>
+        <th>ID</th>
+        <th>Nombre</th>
+        <th>Email</th>
+    </tr>
+
+    <?php foreach ($students as $student): ?>
+        <tr>
+            <td><?php echo $student['id']; ?></td>
+            <td><?php echo $student['name']; ?></td>
+            <td><?php echo $student['email']; ?></td>
+        </tr>
+    <?php endforeach; ?>
+
+</table>
